48a870531b0ace9c830e92fef13595940dea6848
Guavaer <zhaojunjie.mike@gmail.com>
2019-03-09 12:50:46 +0800

    explore: 1 array 2 input 3 form

--- src/App.js
+++ src/App.js
@@ -6,13 +6,39 @@ class App extends Component {
     render() {
         const title = 'This is first line';
         const anotherTitle = 'Another title';
-        return (
-            <div className="App">
-                <h1>
-                    {false ? title : anotherTitle}
-                </h1>
-            </div>
-        );
+        const list = [
+            'Item 1',
+            'Item 2',
+            'Item 3'
+        ];
+        return <div className="App">
+            <h1>
+                {false ? title : anotherTitle}
+
+                {list.map(item => {
+                    return (
+                        <div key={item} onClick={this.onClick}>{item}</div>
+                    );
+                })}
+            </h1>
+            <form onSubmit={this.onSubmit}>
+                <input onChange={this.onChange}></input>
+            </form>
+
+        </div>;
+    }
+
+    onClick() {
+        alert('clicked');
+    }
+
+    onChange(event) {
+        console.log(event.target.value)
+    }
+
+    onSubmit(event) {
+        event.preventDefault();// prevent page from refreshing
+        console.log('submitted!');
     }
 }
 
