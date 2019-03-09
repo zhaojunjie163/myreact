7036fad248dfc11b9ddd90439d244da52bc42dcd
Guavaer <zhaojunjie.mike@gmail.com>
2019-03-09 12:32:42 +0800

    conditional display

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    render() {
+        const title = 'This is first line';
+        const anotherTitle = 'Another title';
+        return (
+            <div className="App">
+                <h1>
+                    {false ? title : anotherTitle}
+                </h1>
+            </div>
+        );
+    }
 }
 
 export default App;
