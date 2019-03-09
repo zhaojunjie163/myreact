19fa554ae189cc78a07f16d8039f81c4e8d33c38
Guavaer <zhaojunjie.mike@gmail.com>
2019-03-09 12:59:38 +0800

    explore: ref

--- src/App.js
+++ src/App.js
@@ -3,42 +3,23 @@ import './App.css';
 
 class App extends Component {
 
-    render() {
-        const title = 'This is first line';
-        const anotherTitle = 'Another title';
-        const list = [
-            'Item 1',
-            'Item 2',
-            'Item 3'
-        ];
-        return <div className="App">
-            <h1>
-                {false ? title : anotherTitle}
+    constructor(props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
 
-                {list.map(item => {
-                    return (
-                        <div key={item} onClick={this.onClick}>{item}</div>
-                    );
-                })}
-            </h1>
+    render() {
+        return <div className="App">
             <form onSubmit={this.onSubmit}>
-                <input onChange={this.onChange}></input>
+                <input ref={input => this.input = input}></input>
             </form>
 
         </div>;
     }
 
-    onClick() {
-        alert('clicked');
-    }
-
-    onChange(event) {
-        console.log(event.target.value)
-    }
-
     onSubmit(event) {
         event.preventDefault();// prevent page from refreshing
-        console.log('submitted!');
+        console.log(this.input.value);
     }
 }
 
